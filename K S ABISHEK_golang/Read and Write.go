a36e31b50c8bca8392f2690dbefc474710a08878
ABISHEK <ABISHEKKS1405@GMAIL.COM>
2025-02-16 22:49:28 +0530

    Create Read and Write.go

---
+++ K S ABISHEK_golang/Read and Write.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func writeFile(filename, content string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer file.Close()
+
+	file.WriteString(content)
+	fmt.Println("File written successfully!")
+}
+
+func readFile(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	fmt.Println("File content:\n", string(data))
+}
+
+func main() {
+	var filename = "sample.txt"
+	var content = "Hello, this is a test file!"
+
+	writeFile(filename, content)
+	readFile(filename)
+}
