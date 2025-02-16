13a4b3f2d1d92e67c580e57d6d032df883e7534f
Siddharthb04 <137971611+Siddharthb04@users.noreply.github.com>
2025-02-16 23:00:02 +0530

    Create stack.go

---
+++ Siddharth_golang/stack.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) Push(item int) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() int {
+	if len(s.items) == 0 {
+		return -1
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+
+func main() {
+	s := Stack{}
+	s.Push(10)
+	s.Push(20)
+	fmt.Println("Popped:", s.Pop())  
+}
